8cfbb5051670c760df724bbb68dce28bd98f5182
Ramansh Chhabra <best_ramansh@hotmail.com>
2021-07-22 15:35:53 -0700

    add defer to script tag, add event listener to div with class one

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@
 //that will log to console the text value of any div that is clicked on inside div .one
 //for example clicking on three will log "three" to the console
 document.querySelector(".one").addEventListener('click', (ev)=>{
-    console.log(ev)
+    console.log(ev.target.textContent)
 })
 
 //part four write a function that takes a name argument and returns a paragraph element
